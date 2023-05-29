2000b54e65ce5693e0b7e9055665528304fb1a59
unknown <it21039386@my.sliit.lk>
2023-05-29 23:37:57 +0530

    portfolio created

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contacts />
+      <SocialLinks />
+      
+
+    </div>
+  );
+}
+
+export default App;
